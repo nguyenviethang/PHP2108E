087fd37a95bb497b6ed02293ff9b2e4b5508b528
thangnv <nguyenvietthangkks@gmail.com>
2023-03-07 12:04:10 +0700

    7/2/2023

---
+++ lession15/unitt01.php
@@ -0,0 +1,27 @@
+<?php
+//tat ca ma lenh php deu trong the mo va the dong nhu tren
+/*comment tren nhieu dong*/
+echo "hello world";
+//echo: hien thi chuoi ra ngoai man hinh
+//https://localhost/php2108/lession15/unit01.php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        /*css*/
+    </style>
+</head>
+
+<body>
+
+</body>
+
+</html>
