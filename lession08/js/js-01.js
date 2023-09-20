dc186e6351c81967053794a0fb5925a2f7842e9f
thangnv <nguyenvietthangkks@gmail.com>
2023-09-20 14:24:13 +0700

    updatecode

---
+++ lession08/js/js-01.js
@@ -0,0 +1,16 @@
+//su dung js de hien thi ra trinh duuyet
+document.write('<h2>hello thang</h2>');
+alert('hi thang');
+
+//hien thi thong bao nhao du lieu
+//prompt('vui long nhap ho ten');
+//hien thi noi dung o man hinh conso debug cua trinh dhuyet 
+//console.log('hello thang');
+
+/**********biến trong JS***************/
+var myName = 'Teo';
+let age = 25;
+
+console.log(myName);
+
+
